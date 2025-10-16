a5786d7cc846843be0792e03fac000179294505d
wallee-deployment-user <technical-systems@wallee.com>
2025-10-16 14:35:18 +0200

    Release 8.8.3

--- src/main/java/ch/postfinance/sdk/DefaultHeaders.java
+++ src/main/java/ch/postfinance/sdk/DefaultHeaders.java
@@ -34,7 +34,7 @@ public class DefaultHeaders {
 
     private HttpHeaders getDefaultHeaders() {
         HttpHeaders headers = new HttpHeaders();
-        headers.put("x-meta-sdk-version", "8.8.1");
+        headers.put("x-meta-sdk-version", "8.8.3");
         headers.put("x-meta-sdk-language", "java");
         headers.put("x-meta-sdk-provider", "PostFinance Checkout");
         headers.put("x-meta-sdk-language-version", System.getProperty("java.version"));
